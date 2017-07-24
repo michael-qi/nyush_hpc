37f8fd87dcc9867ba5ad8e88e78402ed4c0e7a25
Michael Qi <zq4@nyu.edu>
2017-07-24 08:23:06 +0800

    update some parts

--- matlabjob/matlab_parfor.m
+++ matlabjob/matlab_parfor.m
@@ -1,7 +1,7 @@
 % start the matlabpool with maximum available workers
 % control how many workers by setting ntasks in your sbatch script
 pc = parcluster('local')
-matlabpool(pc, getenv('SLURM_CPUS_ON_NODE'))
+parpool(pc, getenv('SLURM_CPUS_ON_NODE'))
 
 % run a parfor loop, distributing the iterations to the 12 workers
 parfor i = 1:100
