f39859c738535cdff904d40f83128fee3a03659e
Michael Qi <zq4@nyu.edu>
2017-09-22 08:58:26 +0800

    Python codes deal the parameter

---
+++ pythonParameter/pythonParameter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys, getopt
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print ('test.py -i <inputfile> -o <outputfile>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print ('test.py -i <inputfile> -o <outputfile>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print ('input file name:', inputfile)
+   print ('output file name:', outputfile)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
